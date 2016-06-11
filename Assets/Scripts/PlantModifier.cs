530dd84c6522c6fa41940c7b3dc7e15fca06abd3
Jordy Kirkman <jordyakirkman@gmail.com>
2016-06-11 17:48:34 -0600

    included all dependancies for monster_plant_1 and organized the assets into their own folder

---
+++ Assets/Scripts/PlantModifier.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlantModifier : MonoBehaviour {
+
+    private float sizeModifier = 0.0F;
+    private Transform plant;
+
+    void Start()
+    {
+        plant = GetComponent<Transform>();
+    }
+
+    void OnTriggerEnter(Collider other) {
+        sizeModifier += 0.01F;
+        if (plant && other.gameObject.CompareTag("PlayerControlled") && sizeModifier < 2) {
+            Debug.Log(sizeModifier);
+            float scaleX = plant.transform.localScale.x + sizeModifier;
+            float scaleY = plant.transform.localScale.y + sizeModifier;
+            float scaleZ = plant.transform.localScale.z + sizeModifier;
+            plant.transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
+        }
+    }
+}
